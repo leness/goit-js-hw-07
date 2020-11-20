d3c22959531a3289e297fa95c757182285e62a37
Olena Stadnyk <lenesx@gmail.com>
2020-11-20 12:38:23 +0200

    fixed homework

--- task-4.js
+++ task-4.js
@@ -7,13 +7,13 @@ let counterValue = 0;
 const increment = () => {
   counterValue += 1;
 
-  document.getElementById('value').textContent = counterValue;
+  span.textContent = counterValue;
 };
 
 const decrement = () => {
   counterValue -= 1;
 
-  document.getElementById('value').textContent = counterValue;
+  span.textContent = counterValue;
 };
 
 btnAdd.addEventListener('click', increment);
--- task-5.js
+++ task-5.js
@@ -2,12 +2,13 @@ let nameInput = document.getElementById("name-input");
 let nameOutput = document.getElementById("name-output");
 
 nameInput.oninput = function () {
+    
     // if (nameInput.value === '') {
     //    nameOutput.innerHTML = 'незнакомец';
     // } else{
     // nameOutput.innerHTML = nameInput.value;
     // }
-    nameInput.value === ''?nameOutput.innerHTML = 'незнакомец':nameOutput.innerHTML = nameInput.value;
-}
 
+    nameOutput.innerHTML = nameInput.value === "" ? "незнакомец" : nameInput.value;
+}
 
