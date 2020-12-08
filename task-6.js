d085254a6b08a1e222ae41b7c6c61290fc98be4f
Olena Stadnyk <lenesx@gmail.com>
2020-12-08 16:51:42 +0200

    add change

--- task-6.js
+++ task-6.js
@@ -1,9 +1,9 @@
-let valueEl = document.getElementById("validation-input")
+const valueEl = document.getElementById("validation-input")
 
 valueEl.onblur = function() {
   console.log(this.value.length);
   
-  if (this.getAttribute('data-length') < this.value.length || this.getAttribute('data-length') > this.value.length) { 
+  if (+this.getAttribute('data-length') !== this.value.length) { 
     this.classList.remove('valid');
     this.classList.add('invalid');
   } else {
